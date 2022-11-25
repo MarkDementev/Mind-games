222fe323f12ff0fb256f8f718d6b52db399c30f5
mark_sur@mail.ru <mark_dementev@DESKTOP-2CE1177.localdomain>
2022-11-25 12:13:33 +0300

    implement arr generation

--- app/src/main/java/hexlet/code/games/Progression.java
+++ app/src/main/java/hexlet/code/games/Progression.java
@@ -1,21 +1,34 @@
 package hexlet.code.games;
 
 import hexlet.code.Engine;
+
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Progression {
     public static void progressionNewRound(String playerName) {
-        int progressionLength = (int) (5 + Math.random() * 10);
-        int emptyPlaceNumber = (int) (Math.random() * (progressionLength + 1));
-        //String firstElementProgression = Engine.calcRandomNumber().toString();
-        String[] progressionArray = new String[progressionLength];
-        //progressionArray[0] = firstElementProgression.toString();
+        int arrLength = (int) (5 + Math.random() * 6);
+        int emptyPlaceNumber = (int) (Math.random() * (arrLength - 1));
+        int progressionStep = 2;
+        int firstElementProgression = Engine.calcRandomNumber();
+        String[] progressionArr = new String[arrLength];
+        int[] intProgressionArr = new int[arrLength];
 
+        for (int i1 = 0; i1 < arrLength; i1++) {
+            intProgressionArr[i1] = firstElementProgression;
+            firstElementProgression = firstElementProgression + progressionStep;
+        }
 
-        Scanner progressionScanner = new Scanner(System.in);
+        for (int i2 = 0; i2 < arrLength; i2++) {
+            progressionArr[i2] = Integer.toString(intProgressionArr[i2]);
+        }
+        progressionArr[emptyPlaceNumber] = "..";
+        System.out.println(Arrays.toString(progressionArr));
+
+        //Scanner progressionScanner = new Scanner(System.in);
         //System.out.println("Question: " + firstOutputElement + " " + secondOutputElement);
-        System.out.print("Your answer: ");
-        String playerAnswer = progressionScanner.next();
+        //System.out.print("Your answer: ");
+        //String playerAnswer = progressionScanner.next();
         //Engine.isCorrectAnswer(playerAnswer, correctAnswer, playerName);
     }
 }
