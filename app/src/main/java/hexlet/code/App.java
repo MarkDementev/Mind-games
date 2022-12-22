2c4c56ce42c1560e686996c885f6db404f485edd
mark_sur@mail.ru <mark_dementev@DESKTOP-2CE1177.localdomain>
2022-12-22 17:42:42 +0300

    make all game types into one System.out.println

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -10,13 +10,7 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         System.out.println("Please enter the game number and press Enter.");
-        System.out.println("1 - Greet");
-        System.out.println("2 - Even");
-        System.out.println("3 - Calc");
-        System.out.println("4 - GCD");
-        System.out.println("5 - Progression");
-        System.out.println("6 - Prime");
-        System.out.println("0 - Exit");
+        System.out.println("1 - Greet\n2 - Even\n3 - Calc\n4 - GCD\n5 - Progression\n6 - Prime\n0 - Exit");
         Scanner playerInteractionScanner = new Scanner(System.in);
         System.out.print("Your choice: ");
         String playerInteractionType = playerInteractionScanner.next();
