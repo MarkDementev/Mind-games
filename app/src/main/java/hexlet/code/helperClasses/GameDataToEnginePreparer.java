ebd50482037dfb9ba3b1cbbe3fcc63ae3c5041c4
mark_sur@mail.ru <mark_dementev@DESKTOP-2CE1177.localdomain>
2022-12-30 19:23:54 +0300

    implement enhanced switch un preparer

--- app/src/main/java/hexlet/code/helperClasses/GameDataToEnginePreparer.java
+++ app/src/main/java/hexlet/code/helperClasses/GameDataToEnginePreparer.java
@@ -8,30 +8,31 @@ import hexlet.code.games.Progression;
 import hexlet.code.games.Prime;
 
 public class GameDataToEnginePreparer {
-    public static String[] gameQuestions = new String[Engine.ROUND_COUNT];
-    public static String[] gameAnswers = new String[Engine.ROUND_COUNT];
     public static void formQuestionsAnswersThenRunEngine(String gameType, String gameRule) {
+        String[] gameQuestions = new String[Engine.ROUND_COUNT];
+        String[] gameAnswers = new String[Engine.ROUND_COUNT];
         for (int i = 0; i < Engine.ROUND_COUNT; i++) {
             switch (gameType) {
-                case "Even":
+                case "Even" -> {
                     gameQuestions[i] = Even.generateEvenQuestion();
                     gameAnswers[i] = Even.generateEvenCorrectAnswer(gameQuestions[i]);
-                    break;
-                case "Calculator":
+                }
+                case "Calculator" -> {
                     gameQuestions[i] = Calculator.generateCalculatorQuestion();
                     gameAnswers[i] = Calculator.generateCalculatorCorrectAnswer(gameQuestions[i]);
-                    break;
-                case "GCD":
+                }
+                case "GCD" -> {
                     gameQuestions[i] = GCD.generateGCDQuestion();
                     gameAnswers[i] = GCD.generateGCDCorrectAnswer(gameQuestions[i]);
-                    break;
-                case "Progression":
+                }
+                case "Progression" -> {
                     gameQuestions[i] = Progression.generateProgressionQuestion();
                     gameAnswers[i] = Progression.generateProgressionCorrectAnswer(gameQuestions[i]);
-                    break;
-                default:
+                }
+                default -> {
                     gameQuestions[i] = Prime.generatePrimeQuestion();
                     gameAnswers[i] = Prime.generatePrimeCorrectAnswer(gameQuestions[i]);
+                }
             }
         }
         Engine.runGame(gameRule, gameQuestions, gameAnswers);
