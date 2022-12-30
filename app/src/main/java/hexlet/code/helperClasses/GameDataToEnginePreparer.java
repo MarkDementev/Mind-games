c0664fadca357cb7629ad5f57e521fb5380f8955
mark_sur@mail.ru <mark_dementev@DESKTOP-2CE1177.localdomain>
2022-12-30 18:44:51 +0300

    simplify preparer

--- app/src/main/java/hexlet/code/helperClasses/GameDataToEnginePreparer.java
+++ app/src/main/java/hexlet/code/helperClasses/GameDataToEnginePreparer.java
@@ -11,36 +11,28 @@ public class GameDataToEnginePreparer {
     public static void formQuestionsAnswersThenRunEngine(String gameType, String gameRule) {
         String[] gameQuestions = new String[Engine.ROUND_COUNT];
         String[] gameAnswers = new String[Engine.ROUND_COUNT];
-        switch (gameType) {
-            case "Even":
-                for (int i = 0; i < Engine.ROUND_COUNT; i++) {
+        for (int i = 0; i < Engine.ROUND_COUNT; i++) {
+            switch (gameType) {
+                case "Even":
                     gameQuestions[i] = Even.generateEvenQuestion();
                     gameAnswers[i] = Even.generateEvenCorrectAnswer(gameQuestions[i]);
-                }
-                break;
-            case "Calculator":
-                for (int i = 0; i < Engine.ROUND_COUNT; i++) {
+                    break;
+                case "Calculator":
                     gameQuestions[i] = Calculator.generateCalculatorQuestion();
                     gameAnswers[i] = Calculator.generateCalculatorCorrectAnswer(gameQuestions[i]);
-                }
-                break;
-            case "GCD":
-                for (int i = 0; i < Engine.ROUND_COUNT; i++) {
+                    break;
+                case "GCD":
                     gameQuestions[i] = GCD.generateGCDQuestion();
                     gameAnswers[i] = GCD.generateGCDCorrectAnswer(gameQuestions[i]);
-                }
-                break;
-            case "Progression":
-                for (int i = 0; i < Engine.ROUND_COUNT; i++) {
+                    break;
+                case "Progression":
                     gameQuestions[i] = Progression.generateProgressionQuestion();
                     gameAnswers[i] = Progression.generateProgressionCorrectAnswer(gameQuestions[i]);
-                }
-                break;
-            default:
-                for (int i = 0; i < Engine.ROUND_COUNT; i++) {
+                    break;
+                default:
                     gameQuestions[i] = Prime.generatePrimeQuestion();
                     gameAnswers[i] = Prime.generatePrimeCorrectAnswer(gameQuestions[i]);
-                }
+            }
         }
         Engine.runGame(gameRule, gameQuestions, gameAnswers);
     }
