1a85075838546bfe38e6f7414de78d691cf87014
mark_sur@mail.ru <mark_dementev@DESKTOP-2CE1177.localdomain>
2023-01-16 18:18:01 +0300

    simplify Even game methods

--- app/src/main/java/hexlet/code/games/Even.java
+++ app/src/main/java/hexlet/code/games/Even.java
@@ -19,16 +19,11 @@ public class Even {
 
         questionAnswerPair[0] = String.valueOf(RandomUtils.generateRandomNumber(RandomUtils.DEFAULT_LOWER_BORDER,
                 RandomUtils.DEFAULT_UPPER_BORDER));
-        questionAnswerPair[1] = isEvenNumber(Integer.parseInt(questionAnswerPair[0]));
+        questionAnswerPair[1] = isEvenNumber(Integer.parseInt(questionAnswerPair[0])) ? "yes" : "no";
         return questionAnswerPair;
     }
 
-    public static String isEvenNumber(int inputNumber) {
-        String correctAnswer = "no";
-
-        if (inputNumber % 2 == 0) {
-            correctAnswer = "yes";
-        }
-        return correctAnswer;
+    public static boolean isEvenNumber(int inputNumber) {
+        return inputNumber % 2 == 0;
     }
 }
